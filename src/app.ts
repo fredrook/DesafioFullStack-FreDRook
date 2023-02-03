d9a087e1225ae2770643b6457729edf3153f7a3d
fredrook <fredericorook@hotmail.com>
2023-02-03 16:35:07 -0300

    Tentando Corrigir Erro de CORS

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import "reflect-metadata";
-import express from "express";
+/* import express from "express"; */
 import "express-async-errors";
 import handleErrorMiddleware from "./Middlewares/handleError.middleware";
 import userRoutes from "./Routes/user.routes";
 import loginRouter from "./Routes/login.routes";
 import contactsRoutes from "./Routes/contacts.routes";
 
+var express = require('express')
 var cors = require('cors')
+var app = express()
 
-const app = express();
-app.use(express.json(cors));
+app.use(cors);
 
 app.use("/users", userRoutes);
 app.use("/login", loginRouter);
