de5b606962b3cb54455bdf3dfe22b256067cb3b3
fredrook <fredericorook@hotmail.com>
2023-02-06 11:09:08 -0300

    Refatorando

--- src/Services/login/loginUser.service.ts
+++ src/Services/login/loginUser.service.ts
@@ -1,51 +1,52 @@
-import { compare } from "bcryptjs"
-import AppDataSource from "../../data-source"
-import Users from "../../Entities/User.Entity"
-import AppError from "../../Error/AppError"
-import { ILogin } from "../../Interfaces/ILogin"
-import jwt from "jsonwebtoken"
-import "dotenv/config"
+import { compare } from "bcryptjs";
+import AppDataSource from "../../data-source";
+import Users from "../../Entities/User.Entity";
+import AppError from "../../Error/AppError";
+import { ILogin } from "../../Interfaces/ILogin";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const loginUserService = async ({ email, password }: ILogin) => {
-  const userRepository = AppDataSource.getRepository(Users)
+  const userRepository = AppDataSource.getRepository(Users);
 
   if (!email) {
-    throw new AppError("Email is a required information")
+    throw new AppError("Email is a required information");
   }
 
   if (!password) {
-    throw new AppError("Password is a required information")
+    throw new AppError("Password is a required information");
   }
 
   const user = await userRepository.findOneBy({
     email,
-  })
+  });
 
   if (!user) {
-    throw new AppError("Wrong password or email", 403)
+    throw new AppError("Wrong password or email", 403);
   }
 
   if (!user.isActive) {
-    throw new AppError("User is currently inactive", 403)
+    throw new AppError("User is currently inactive", 403);
   }
 
-  const matchUser = await compare(password, user.password)
+  const matchUser = await compare(password, user.password);
   if (!matchUser) {
-    throw new AppError("Wrong password or email", 403)
+    throw new AppError("Wrong password or email", 403);
   }
 
   const token = jwt.sign(
     {
       isAdmin: user.isAdmin,
+      email: user.email,
     },
     process.env.SECRET_KEY as string,
     {
       expiresIn: "2h",
       subject: user.id,
-    },
-  )
+    }
+  );
 
-  return token
-}
+  return token;
+};
 
-export default loginUserService
+export default loginUserService;
