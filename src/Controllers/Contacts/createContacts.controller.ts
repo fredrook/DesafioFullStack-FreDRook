694a532163a1defc5cccea351ada7032f9e34610
fredrook <fredericorook@hotmail.com>
2023-02-06 07:58:20 -0300

    Refatorando Create Contacts para eliminar Bugs

--- src/Controllers/Contacts/createContacts.controller.ts
+++ src/Controllers/Contacts/createContacts.controller.ts
@@ -3,11 +3,11 @@ import { instanceToPlain } from "class-transformer"
 import { IContactsRequest } from "../../Interfaces/IContacts"
 import createContactService from "../../Services/Contacts/createContacts.service"
 
-const createSpecialtyController = async (req: Request, res: Response) => {
+const createContactController = async (req: Request, res: Response) => {
   const createContact: IContactsRequest = req.body
 
   const newSpecialty = await createContactService(createContact)
 
   return res.status(201).json(instanceToPlain(newSpecialty))
 }
-export default createSpecialtyController
+export default createContactController
